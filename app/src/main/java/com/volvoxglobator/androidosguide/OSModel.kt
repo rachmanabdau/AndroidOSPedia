fe3cece60ad8d02e486bfc45c524b58c778a3d5c
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2019-08-10 09:28:57 +0700

    Create Model and String Resource.

---
+++ app/src/main/java/com/volvoxglobator/androidosguide/OSModel.kt
@@ -0,0 +1,6 @@
+package com.volvoxglobator.androidosguide
+
+data class OSModel(val img: Int,
+                   val versionName: String,
+                   val apiLevel: String,
+                   val description: String)
