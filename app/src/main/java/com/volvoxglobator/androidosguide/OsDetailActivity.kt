80103b1140a36b985865f0d7678fd8ae7278b86c
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2019-08-10 10:37:22 +0700

    Create Detail Activity Layout.

---
+++ app/src/main/java/com/volvoxglobator/androidosguide/OsDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.volvoxglobator.androidosguide
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OsDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_os_detail)
+    }
+}
